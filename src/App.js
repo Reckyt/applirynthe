0d32fbac7d28adef16d39a88652d3abae3632681
Maxime <maxime.lombardo@gmail.com>
2019-10-30 16:48:11 +0100

    routing version 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./components/home/Home";
+import Map from "./components/map/Map";
+import Enigma1 from "./components/enigma/Enigma1";
+import Monster1 from "./components/monsters/Monster1";
+import Wall1 from "./components/walls/Wall1";
+import Exit from "./components/exit/Exit";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/map/Map" component={Map} />
+        <Route path="/enigma/Enigma1" component={Enigma1} />
+        <Route path="/monsters/Monster1" component={Monster1} />
+        <Route path="/walls/Wall1" component={Wall1} />
+        <Route path="/exit/Exit" component={Exit} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import "../../styles/css/home/Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <button className="home_play">Play</button>
+      </div>
+    );
+  }
+}
