54e87f129253f9c866f1904ce05591818c68dda1
Maxime <maxime.lombardo@gmail.com>
2019-10-30 21:53:21 +0100

    structur page home fini

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 
+import jigsaw from "../../styles/assets/jigsaw.png";
 import "../../styles/css/home/Home.css";
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <button className="home_play">Play</button>
+      <div className="home">
+        <img className="jigsaw" src={jigsaw} alt="jigsaw" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          required
+          minlength="4"
+          maxlength="42"
+          placeholder="Quel est ton nom mon mignon ?"
+        />
+        <button className="home_play">Viens jouer avec moi</button>
       </div>
     );
   }
