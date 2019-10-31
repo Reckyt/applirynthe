d8985dbadcaf93718b0215d0a893dde6b358c994
Maxime <maxime.lombardo@gmail.com>
2019-10-31 05:31:35 +0100

    add wall incassable

---
+++ src/components/walls/ReturnMap.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import "../../styles/css/walls/ReturnMap.css";
+
+export default class Wall3 extends Component {
+  render() {
+    return (
+      <div className="wall">
+        <button className="retour">Retour à la map</button>
+      </div>
+    );
+  }
+}
